de80a366da2510bd97f090216454ec4ebc1be147
jjun-ho <gkswnsgh2@naver.com>
2022-03-30 13:17:55 +0900

    2022.3.30

--- 2. 정렬/7_기수_정렬.py
+++ 2. 정렬/7_기수_정렬.py
@@ -1,5 +1,33 @@
+import random
+from timeit import default_timer as timer
 
+def rsort(A, m):
+    buckets =  [[] for _ in range(10)]
+    for v in A:
+        index = v // (10 ** m)
+        index %= 10
+        buckets[index].append(v)
+    res = []
+    for bucket in buckets:
+        res.extend(bucket)
+    return res
 
+def radix_sort(A, k):
+    for i in range(0, k):
+        A = rsort(A, i)
+    return A
+
+def test(A):
+    for i in range(1, len(A)):
+        if A[i-1] > A[i]:
+            return False
+    return True
+
+x = random.sample(range(10000), 100) start = timer()
+x = radix_sort(x, 4)
+print(timer() - start)
+print(x)
+print(test(x))
 """
 * 기수 정렬(Radix Sort)
 - 입력이 모두 k 자릿수 이하의 자연수인 경우
